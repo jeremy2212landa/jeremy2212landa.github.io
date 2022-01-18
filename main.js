cad5699477516185342b40284992c7ad108775c9
jeremy2212landa <jeremy2212landa@gmail.com>
2022-01-18 02:27:39 -0400

    commit

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log("Hola como Estamos");
+
+let mainevent = document.getElementById('logo');
+
+document.addEventListener('click', (e) => {
+  if (!e.target.matches("img")) return false;
+  window.location.href = "./#skills";
+});
