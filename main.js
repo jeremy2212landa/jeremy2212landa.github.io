808206cd26ba3ec06b85b48d74c19494bd310f5d
jeremy2212landa <jeremy2212landa@gmail.com>
2022-02-09 01:16:05 -0400

    añadiendo js para el menu hamburguesa

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ document.addEventListener('click', (e) => {
   if (!e.target.matches("img")) return false;
   window.location.href = "./#home";
 });
+
+  let tt = document.getElementById('text');
+  let hamburger = document.getElementById('hamburger');
+  document.addEventListener('click', (e) => {
+    if (!e.target.matches(".menutoggle")) {
+      tt.classList.remove("active");
+      tt.classList.add("menucontent");
+      return false;
+    }
+    tt.classList.toggle("menucontent");
+    tt.classList.toggle("active");
+    console.log(tt);
+    // if (!e.target.matches("hamburger")) return false;
+
+  });
