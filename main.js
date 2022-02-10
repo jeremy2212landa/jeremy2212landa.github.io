276333323ee4fd5940dd3f5719b2d78593f0f0c2
jeremy2212landa <jeremy2212landa@gmail.com>
2022-02-09 22:39:34 -0400

    canvas de dots & slash y ajuste de queries skills

--- main.js
+++ main.js
@@ -21,3 +21,50 @@ document.addEventListener('click', (e) => {
     // if (!e.target.matches("hamburger")) return false;
 
   });
+
+
+  //Matrix/////////////////////
+
+  var c = document.getElementById("pattern");
+var ctx = c.getContext("2d");
+c.height = window.innerHeight;
+c.width = window.innerWidth;
+var font_size = 30;
+var columns = c.width / font_size;
+var character = ".I";
+character = character.split("");
+var drops = [];
+
+function init() {
+	drops = [];
+	for (var x = 0; x < columns; x++) drops[x] = 1;
+}
+
+function draw() {
+	ctx.fillStyle = "#2256cb";
+	ctx.fillRect(0, 0, c.width, c.height);
+	ctx.fillStyle = "#0F0";
+	ctx.font = font_size + "px Varela Round";
+	for (var i = 0; i < drops.length; i++) {
+		var text = character[Math.floor(Math.random() * character.length)];
+		ctx.fillText(text, i * font_size, drops[i] * font_size);
+		if (drops[i] * font_size > c.height && Math.random() > 0.975) drops[i] = 0;
+		drops[i]++;
+	}
+}
+
+init();
+let raining = setInterval(draw, 33);
+
+const handleResize = () => {
+	clearInterval(raining);
+	c.height = window.innerHeight;
+	c.width = window.innerWidth;
+	columns = c.width / font_size;
+	c.setAttribute("width", c.width);
+	c.setAttribute("height", c.height);
+	init()
+	raining = setInterval(draw, 33);
+};
+
+window.addEventListener("resize", handleResize);
